b4db3e7212eec7ca794ceddae2bda32d77c04a8a
shengwen <shengwen2000@gmail.com>
2024-08-07 15:50:35 +0800

    default api 擴充快速方法

--- src/EasyApiProxy/DefaultApiResult.cs
+++ src/EasyApiProxy/DefaultApiResult.cs
@@ -8,6 +8,46 @@ namespace EasyApiProxys
     public class DefaultApiResult
     {
         /// <summary>
+        /// apiResult builder
+        /// </summary>
+        public static DefaultApiResult FromOK()
+        {
+            return new DefaultApiResult { Result = "OK", Message = "Success" };
+        }
+
+        /// <summary>
+        /// apiResult builder
+        /// </summary>
+        public static DefaultApiResult From(string result, string message)
+        {
+            return new DefaultApiResult { Result = result, Message = message };
+        }
+
+        /// <summary>
+        /// apiResult builder
+        /// </summary>
+        public static DefaultApiResult From(string result, string message, object data)
+        {
+            return new DefaultApiResult { Result = result, Message = message, Data = data };
+        }
+
+        /// <summary>
+        /// apiResult builder
+        /// </summary>
+        public static DefaultApiResult<T> From<T>(string result, string message, T data)
+        {
+            return new DefaultApiResult<T> { Result = result, Message = message, Data = data };
+        }
+
+        /// <summary>
+        /// apiResult builder
+        /// </summary>
+        public static DefaultApiResult FromException(ApiCodeException apiCodeException)
+        {
+            return new DefaultApiResult { Result = apiCodeException.Code, Message = apiCodeException.Message };
+        }
+
+        /// <summary>
         /// 回傳代碼 OK 正常 其他為例外代碼
         /// </summary>
         [JsonProperty(Order=1)]
