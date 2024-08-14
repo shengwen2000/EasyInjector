8084482bb434b35c9fda379ac5840304e8ab80a5
shengwen <shengwen2000@gmail.com>
2024-08-14 12:22:29 +0800

    EasyInjector.Dev 泛型覆蓋 方法

--- src/EasyInjector.Dev/EasyInjectorDevExtension.cs
+++ src/EasyInjector.Dev/EasyInjectorDevExtension.cs
@@ -88,5 +88,85 @@ namespace EasyInjectors
                 return proxy;
             }
         }
+
+        /// <summary>
+        /// 增加複寫服務(泛型)
+        /// </summary>     
+        /// <param name="injector">Injector</param>
+        /// <param name="serviceType">要複寫的服務</param>
+        /// <param name="overrideType">要複寫類別 (方法須標示[Override]才會複蓋)</param>
+        /// <returns></returns>
+        public static void AddOverrideGeneric(this EasyInjector injector, Type serviceType, Type overrideType)
+        {
+            // 前一個
+            var r1 = injector.FindRegister(serviceType);
+            if (r1 == null)
+                throw new NotSupportedException(string.Format("服務{0}沒有註冊", serviceType.FullName));
+
+            if (r1.IsGeneric==false)
+                throw new NotSupportedException("此方法僅支援泛型服務");
+
+            injector.AddGenericService(r1.Lifetimes, serviceType, (sp, targs) =>
+                CreateOverrideGenericInstance(sp, r1, serviceType, overrideType, targs));
+        }
+
+        /// <summary>
+        /// 建立複寫服務實例
+        /// </summary>     
+        /// <param name="provider">服務提供</param>
+        /// <param name="baseServiceRegister">基礎服務註冊</param>
+        /// <param name="typeArgs">泛型參數</param>
+        /// <param name="serviceType">服務類別</param>
+        /// <param name="overrideType">要覆蓋的類別</param>
+        /// <returns>複寫服務</returns>
+        static object CreateOverrideGenericInstance(IServiceProvider provider, ServiceRegister baseServiceRegister, Type serviceType, Type overrideType, Type[] typeArgs)
+        {
+            var overrideType1 = overrideType.MakeGenericType(typeArgs);
+            var ctor1 = overrideType1.GetConstructors()
+                .Where(x => x.IsPublic)
+                .FirstOrDefault();
+
+            if (ctor1 == null)
+                throw new ApplicationException(string.Format("類別{0}沒有公開建構子，無法生成實例", overrideType1.FullName));
+
+            var serviceType1 = serviceType.MakeGenericType(typeArgs);
+
+            // 建構參數
+            var pp = ctor1.GetParameters();
+            var vv = new object[pp.Length];
+
+            // 逐一取的建構參數服務
+            for (var i = 0; i < pp.Length; i++)
+            {
+                var p1 = pp[i];
+                // 如果是服務自身的話 提供舊的版本
+                object srv1 = null;
+                if (p1.ParameterType == serviceType1)
+                    srv1 = baseServiceRegister.CreateGenericFunc(provider, typeArgs);
+                else
+                    srv1 = provider.GetService(p1.ParameterType);
+                if (srv1 == null)
+                    throw new ApplicationException(string.Format("類別{0} 要求注入服務{1} 失敗", overrideType.FullName, p1.ParameterType.FullName));
+                vv[i] = srv1;
+            }
+
+            {
+                // 複寫的服務實例
+                var overOne = ctor1.Invoke(vv);
+                if (overOne == null)
+                    throw new ApplicationException(string.Format("類別{0} 無法生成實例", overrideType.FullName));
+
+                // 基礎的服務實例
+                object baseOne = baseServiceRegister.CreateGenericFunc.Invoke(provider, typeArgs);
+                if (baseOne == null)
+                    throw new ApplicationException(string.Format("類別{0}的複寫基礎服務 無法生成實例", overrideType.FullName));
+
+                // 產生代理類別
+                var generator = new ProxyGenerator();
+                var proxy = generator.CreateInterfaceProxyWithoutTarget(serviceType1, new OverrideInterceptor(overOne, baseOne));
+                return proxy;
+            }
+        }
+
     }
 }
---
+++ src/EasyInjector.Tests/OverrideGenericTest.cs
@@ -0,0 +1,104 @@
+﻿using EasyInjectors;
+using EasyInjectors.Dev;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tests.OverrideGenerics;
+
+namespace Tests
+{
+    [Category("Common")]
+    [TestFixture]
+    public class OverrideGenericTest : BaseTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            //每個測試方法執行前都會執行的動作
+        }
+
+        [Test]
+        public async void OverrideGeneric001()
+        {
+            var injector = new EasyInjector();
+
+            // add generic service
+            injector.AddGenericService(SimpleLifetimes.Singleton, typeof(ILogger<>), typeof(DefaultLogger<>));
+
+            // 複寫測試類別
+            injector.AddOverrideGeneric(typeof(ILogger<>), typeof(ConsoleLogger<>));
+
+            var logger = injector.GetRequiredService<ILogger<Log>>();
+            logger.AddLog(new Log { Message = "Demo1" });
+
+            Assert.True(Log.Messages[0] == "Demo1");
+            Assert.True(Log.Messages[1] == "(Console)Demo1");
+            Log.Messages.Clear();
+
+            await logger.AddLogAsync(new Log { Message = "Demo2" });
+            Assert.True(Log.Messages[0] == "(Console Async)Demo2");
+        }        
+    }
+
+    namespace OverrideGenerics
+    {
+        public class Log
+        {
+            public static List<string> Messages = new List<string>(10);
+
+            public string Message { get; set; }
+
+            public override string ToString()
+            {
+                return Message;
+            }
+        }
+
+        public interface ILogger<TLOG> where TLOG:Log
+        {
+            void AddLog(TLOG log);
+
+            Task AddLogAsync(TLOG log);
+        }
+
+        public class DefaultLogger<TLOG> : ILogger<TLOG> where TLOG : Log
+        {
+            public void AddLog(TLOG log)
+            {
+                Log.Messages.Add(log.ToString());
+            }
+
+
+            public async Task AddLogAsync(TLOG log)
+            {
+                await Task.Delay(500);
+                Log.Messages.Add(string.Format("(Async){0}", log.ToString()));
+            }
+        }
+
+        public class ConsoleLogger<TLOG> : ILogger<TLOG> where TLOG : Log
+        {
+            private ILogger<TLOG> _baseLogger;
+
+            public ConsoleLogger(ILogger<TLOG> baseLogger)
+            {
+                _baseLogger = baseLogger;
+            }
+
+            [Override]
+            public void AddLog(TLOG log)
+            {
+                _baseLogger.AddLog(log);
+                Log.Messages.Add(string.Format("(Console){0}", log.ToString()));
+            }
+
+
+            [Override]
+            public async Task AddLogAsync(TLOG log)
+            {
+                await Task.Delay(500);
+                Log.Messages.Add(string.Format("(Console Async){0}", log.ToString()));
+            }
+        }
+    }
+}
