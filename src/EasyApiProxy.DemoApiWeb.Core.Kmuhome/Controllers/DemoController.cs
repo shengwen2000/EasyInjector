069520b4dda4239e18a56548532495f2de2943f2
shengwen <shengwen2000@gmail.com>
2025-02-05 17:37:41 +0800

    Kmuhome Api 專屬測試網站

---
+++ src/EasyApiProxy.DemoApiWeb.Core.Kmuhome/Controllers/DemoController.cs
@@ -0,0 +1,128 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using EasyApiProxys;
+using EasyApiProxys.DemoApis;
+using EasyApiProxys.WebApis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyApiProxy.DemoApiWeb.Controllers
+{
+    /// <summary>
+    /// Demo Api
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    // 使用 KmuhomeApi 封裝回應
+    [KmuhomeApiResult]
+    public class DemoController : ControllerBase, IDemoApi
+    {
+        public DemoController()
+        {
+        }
+
+        [HttpGet]
+        public string Ping()
+        {
+            return "Hello Ping";
+        }
+
+        [HttpPost]
+        public async Task<AccountInfo> Login(Login req)
+        {
+            await Task.Delay(1000);
+            if (req.Account == "david" && req.Password == "123")
+            {
+                return new AccountInfo
+                {
+                    Account = req.Account,
+                    Token = "123456789",
+                    Expired = DateTime.Now.AddHours(1),
+                    Roles = [Roles.AdminUser]
+                };
+            }
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task Logout(TokenInfo req)
+        {
+            await Task.Delay(1000);
+            if (req.Token == "123456789")
+                return;
+            throw new ApplicationException("The Token Not exists");
+        }
+
+        [HttpPost]
+        public async Task<string> GetEmail(TokenInfo req)
+        {
+            await Task.Delay(1000);
+            if (req.Token == "123456789")
+            {
+                return "david@gmail.com";
+            }
+            throw new ApplicationException("The Token Not exists");
+        }
+
+        [HttpPost]
+        public string GetServerInfo()
+        {
+            return "Demo Server";
+        }
+
+        [HttpPost]
+        public Task<string> RunProc(ProcInfo req)
+        {
+            return RunProc_001(req);
+        }
+
+        [HttpPost]
+        public async Task<string> RunProc_001(ProcInfo req)
+        {
+            if (req.ProcSeconds > 0)
+                await Task.Delay(TimeSpan.FromSeconds(req.ProcSeconds));
+            return string.Format("OK {0}", req.ProcSeconds);
+        }
+
+        [HttpPost]
+        public Task RaiseValidateError()
+        {
+            var info = new AccountInfo();
+            throw GenPropError(info, o => o.Account, "帳號為必要");
+        }
+
+        [HttpPost]
+        public void NoResult()
+        {
+        }
+
+        [HttpPost]
+        public Task NoResult2()
+        {
+            return Task.CompletedTask;
+        }
+
+        [Authorize(AuthenticationSchemes = "Hawk", Roles = "Admins")]
+        [HttpPost]
+        public async Task<string> HawkApi()
+        {
+            await Task.FromResult(0);
+            return "hawk api ok";
+        }
+
+         /// <summary>
+        /// 產生屬性驗證錯誤
+        /// - thorw ValidationException
+        /// </summary>
+        static public ValidationException GenPropError<T>(T obj, Expression<Func<T, object?>> theProp, string? errorMessage) {
+
+            if(theProp.Body is MemberExpression mb1) {
+                var result = new ValidationResult(errorMessage, [mb1.Member.Name]);
+                return new ValidationException(result, null, obj);
+            }
+            else
+                throw new ApplicationException($"{nameof(theProp)} 必須選擇成員屬性");
+        }
+    }
+}
---
+++ src/EasyApiProxy.DemoApiWeb.Core.Kmuhome/Program.cs
@@ -0,0 +1,62 @@
+using UoN.ExpressiveAnnotations.NetCore.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 授權服務註冊
+builder.Services.AddAuthentication()
+    // 註冊Hawk驗證
+    .AddHawkAuthorize(opt =>
+    {
+        // 設定證書
+        opt.Credentials.Add(new HawkNet.AspNetCore.HawkCredential
+        {
+            Id = "123",
+            Key = "werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn",
+            Algorithm = "sha256",
+            User = "Admin",
+            Roles = ["Admins"]
+        });
+
+        // 也可以自行實作取得證書方法
+        //opt.GetCredentialFunc = (id) => Task.FromResult(new HawkNet.AspNetCore.HawkCredential());
+    });
+
+builder.Services.AddControllers()
+    // 使用 KmuhomeApi 協助 json 選項
+    .AddKmuhomeApiJsonOptions();
+
+// requiredif 這種的
+builder.Services.AddExpressiveAnnotations();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+// 使用路由
+app.UseRouting();
+
+// 啟用路由後 底下這種的就可以執行
+//app.MapGet("/hello/{name:alpha}", (string name) => $"Hello {name}!");
+
+// 啟用憑證檢驗 確定憑證有效性
+app.UseAuthentication();
+// 啟用權限驗證機制 檢查你是否有權限存取方法
+app.UseAuthorization();
+
+// 啟用 API Controllers
+app.MapControllers();
+
+app.Run();
+
