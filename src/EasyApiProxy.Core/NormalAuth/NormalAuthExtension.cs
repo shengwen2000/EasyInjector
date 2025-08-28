1118da4ffececb21aad140424662c09b2d216139
shengwen <shengwen2000@gmail.com>
2025-08-28 11:17:28 +0800

    add missing files

---
+++ src/EasyApiProxy.Core/NormalAuth/NormalAuthExtension.cs
@@ -0,0 +1,39 @@
+﻿
+using EasyApiProxys.NormalAuth;
+using System.Net.Http.Headers;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace EasyApiProxys;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+/// <summary>
+/// 用戶端啟用一般驗證
+/// - 自行指定 Http Header Authorization 內容
+/// </summary>
+public static class NormalAuthExtension
+{
+    /// <summary>
+    /// 用戶端啟用一般驗證
+    /// - 自行指定 Http Header Authorization 內容
+    /// </summary>
+    /// <param name="builder">ProxyBuilder</param>
+    /// <param name="authValue">指定 Http Header Authorization 內容 e.g. Basic QVBJOjg1MTU2MTE0MzE1MTYxNjg=</param>
+    static public ApiProxyBuilder UseAuthorizationHeader(this ApiProxyBuilder builder, string authValue)
+    {
+        var hander = new MethodHandler(authValue, builder.Options.GetHttpMessageHandler);
+        builder.Options.GetHttpMessageHandler = hander.GetHandler;
+        return builder;
+    }
+
+    internal class MethodHandler(
+        string authValue,
+        Func<HttpMessageHandler> current)
+    {
+        readonly private AuthenticationHeaderValue _authValue = AuthenticationHeaderValue.Parse(authValue);
+
+        public HttpMessageHandler GetHandler()
+        {
+            return new NormalClientMessageHandler(current.Invoke(), _authValue);
+        }
+    }
+}
---
+++ src/EasyApiProxy.Core/NormalAuth/NormalClientMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+
+namespace EasyApiProxys.NormalAuth
+{
+    /// <summary>
+    /// HttpClient's message handler for Basic authentication.
+    /// </summary>
+    /// <remarks>
+    ///   HttpClient's message handler for Basic authentication.
+    ///   <param name="authHeaderValue">e.g. Basic QVBJOjg1MTU2MTE0MzE1MTYxNjg=</param>
+    ///   <param name="innerHandler">inner Handler</param>
+    /// </remarks>
+    public class NormalClientMessageHandler(HttpMessageHandler innerHandler, AuthenticationHeaderValue authHeaderValue) : DelegatingHandler(innerHandler)
+    {
+
+        /// <summary>
+        /// 作為異步操作，將 HTTP 請求發送到內部處理程序以發送到服務器。
+        /// 在發送請求前添加 Basic 認證頭。
+        /// </summary>
+        /// <param name="request">要發送到服務器的 HTTP 請求消息。</param>
+        /// <param name="cancellationToken">用於取消操作的取消標記。</param>
+        /// <returns>從服務器接收的 HTTP 響應消息。</returns>
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = authHeaderValue;
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
